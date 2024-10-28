2e4c82998a2ac18d1c963eb1e40402dfe614f601
matnaryo <rohmat.sunaryo@gmail.com>
2024-10-28 20:16:34 +0700

    28/10/2024 1

--- perulangan.py
+++ perulangan.py
@@ -1,5 +1,5 @@
 bil = int(input("Masukkan bilangan :"))
 if (bil >= 0):
-    print("Anad memasukkan bilangan non negatif")
+    print("Anda memasukkan bilangan non negatif")
 else:
     print("Anda memasukkan bilangan negatif")
